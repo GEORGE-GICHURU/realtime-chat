b2b968c777cf0c61998a0873f4a216b4f0823082
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-04-20 20:46:32 +0300

    Tailwind clear

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
+import { db } from "@/lib/db"
+import Button from "@/components/ui/Button"
 
-
-export default function Home() {
-  return (
-    <div className="text-red-500">Hello World</div>
-
-  )
+export default async function Home() {
+  return <Button>Hello world</Button>
 }
